870479a6c48e81d5cfb56f8b0cbf5c99aa98f9b8
Firefly4266 <gaines.johnp@gmail.com>
2017-08-24 21:25:21 -0700

    Finished coding command loop demo

---
+++ CommandLoop/CommandLoop.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class CommandLoop
+{
+    public static void Main(string[] args)
+    {
+        InputWrapper iw = new InputWrapper();
+        Console.WriteLine("Enter command, 'quit' to exit.");
+        string cmd = iw.getString("> ");
+        while (!cmd.Equals("quit"))
+        {
+            if (cmd.Equals("Length"))
+            {
+                Console.WriteLine(cmd);
+                break;
+            }
+            else if (cmd.Equals("new"))
+            {
+                Console.WriteLine(cmd);
+                break;
+            }
+            else if (cmd.Equals("show"))
+            {
+                Console.WriteLine(cmd);
+                break;
+            }
+            else
+            {
+                cmd = iw.getString(">> ");
+            }
+        }
+    }
+}
